8f973532d78afd5b3bfefdad4c35444db6d1485e
Vyomesh P V <vyomeshpv@gmail.com>
2016-03-22 13:07:35 -0700

    Removed hardcode and

--- server/api/sms/sms.controller.js
+++ server/api/sms/sms.controller.js
@@ -57,13 +57,13 @@ var validateSMS= function(message){
     var type="CodeReplyMsg";
     var smsbody=message.body.toUpperCase().trim();
     var phone=message.from;
-    var from=phone.substring((phone.length-10), phone.length);from="1234567890";
+    var from=phone.substring((phone.length-10), phone.length);
     var isReceiver= User.findOne({"foodRecoveryInfo.foodRecoveryContactPhone":from});
-    var isValidCode= Transaction.findOne({code:smsbody});
-    var isValidCodeReceiver= Transaction.findOne({code:smsbody,"receivers.phone":from},
+    var isValidCode= Transaction.findOne({code:smsbody,transactionDate:{ $lte:moment().format(), $gt:moment().subtract('24','hours').format()}});
+    var isValidCodeReceiver= Transaction.findOne({code:smsbody,"receivers.phone":from,transactionDate:{ $lte:moment().format(), $gt:moment().subtract('24','hours').format()}},
         {itemDescription:1,donationStatus:1,code:1,donor:1,transactionDate:1,transactionId:1,'receivers.$':1,'acceptor':1} )
     .populate('receivers.receiverId donor.donorId acceptor.receiverId');//check inprogress or completed  or if same from then resend details.
-    var isCodeExpiredElseUpdate=Transaction.findOne({code:smsbody,donationStatus:'Inprogress',transactionDate:{ $lt:moment().format()},"receivers.phone": "1234567890"},
+    var isCodeExpiredElseUpdate=Transaction.findOne({code:smsbody,donationStatus:'Inprogress',transactionDate:{ $lte:moment().format(), $gt:moment().subtract('24','hours').format()},"receivers.phone": from},
                                 {itemDescription:1,donationStatus:1,code:1,donor:1,transactionDate:1,transactionId:1,'receivers.$':1,$isolated : 1} )
                                 .populate('receivers.receiverId donor.donorId');
     var isReceiverMsg="You are not a valid receiver";
